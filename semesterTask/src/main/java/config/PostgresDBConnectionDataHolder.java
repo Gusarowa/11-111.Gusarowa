2b760c7b4d3c595540badaf562f7e069fa98eca0
Gusarowa <89986715+Gusarowa@users.noreply.github.com>
2022-12-09 19:16:58 +0300

    Delete semesterTask directory

--- semesterTask/src/main/java/config/PostgresDBConnectionDataHolder.java
+++
@@ -1,8 +0,0 @@
-package config;
-
-public class PostgresDBConnectionDataHolder {
-    public static final String URL = "jdbc:postgresql://localhost:5432/online-gym";
-    public static final String DRIVER = "org.postgresql.Driver";
-    public static final String USERNAME = "lenagusarova";
-    public static final String PASSWORD = "qwerty";
-}
